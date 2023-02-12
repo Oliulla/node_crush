8e8a8cee57878179ad322e0e64410fe43df7f660
oliullah <razasm29@gmail.com>
2023-02-12 16:24:01 +0600

    http req

--- index.js
+++ index.js
@@ -32,14 +32,78 @@
 
 // ---------callback fn--------------
 
-function greet(name) {
-  console.log(`Hello, ${name}`);
-}
+// function greet(name) {
+//   console.log(`Hello, ${name}`);
+// }
 
-function red(greetFn) {
-    
-  const name = "Red";
-  return greetFn(name);
-}
+// function red(greetFn) {
 
-red(greet);
+//   const name = "Red";
+//   return greetFn(name);
+// }
+
+// red(greet);
+
+// const fs = require("node:fs/promises");
+
+// fs.readFile("file.txt", "utf-8")
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// console.log("first");
+// async function readFile() {
+//   try {
+//     const data = await fs.readFile("file.txt", "utf-8");
+//     console.log(data);
+//     console.log("second");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// console.log("third");
+// readFile();
+
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  // res.end(req.url);
+
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/josn" });
+    res.end(
+      JSON.stringify({
+        firstName: "Bruce",
+        lastName: "Lee",
+      })
+    );
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("page not found!");
+  }
+
+  // const superHero = {
+  //   firstName: "Dawyne",
+  //   lastName: "Bruce",
+  // };
+
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // fs.createReadStream("./index.html", "utf-8").pipe(res);
+
+  // const html = fs.readFileSync("./index.html", "utf-8");
+  // res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("I am listening port 3000");
+});
