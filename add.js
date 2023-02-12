f84bcdf9410010573ae2a3303111e9c3df749a76
oliullah <razasm29@gmail.com>
2023-02-12 12:56:58 +0600

    node practice

---
+++ add.js
@@ -0,0 +1,6 @@
+const sum = (a, b) => {
+  return a + b;
+};
+// console.log(sum(3, 4));
+
+module.exports = sum;
---
+++ index.js
@@ -0,0 +1,19 @@
+// const sum = require("./add");
+// require("./batman");
+// require("./superman");
+
+// // console.log("hello from index js");
+// // console.log(sum(3, 4));
+
+// const superHero = require("./super_hero");
+// console.log(superHero.getName());
+// superHero.setName("Superman");
+// console.log(superHero.getName());
+
+// const superHeroName = require("./super_hero");
+
+// superHeroName.setName("Superman");
+// console.log(superHeroName.getName());
+
+// superHeroName.setName("Batman");
+// console.log(superHeroName.getName(superHeroName));
